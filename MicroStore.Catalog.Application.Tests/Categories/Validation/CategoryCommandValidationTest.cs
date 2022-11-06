8361d9340cc838f59ef2900787f9f1f0acdd22ea
Mohamed Gamal <mohamedsherf17@gmail.com>
2022-11-07 00:52:43 +0200

    Add Category Validation Test

---
+++ MicroStore.Catalog.Application.Tests/Categories/Validation/CategoryCommandValidationTest.cs
@@ -0,0 +1,74 @@
+﻿using FluentAssertions;
+using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+using MicroStore.Catalog.Application.Abstractions.Categories.Commands;
+
+
+namespace MicroStore.Catalog.Application.Tests.Categories.Validation
+{
+    public class CategoryCommandValidationTest : BaseTestFixture
+    {
+        [Test]
+        [TestCase(null, Reason = "CategoryNameIsNull")]
+        [TestCase("aa", Reason = "CategoryNameLenghtIsNotMetWithMinimumRequiredLenght")]
+        public async Task ShouldFailWhenCategoryNameIsNotValid(string name)
+        {
+            using (var scope = ServiceProvider.CreateScope())
+            {
+                var sut = scope.ServiceProvider.GetRequiredService<IValidator<FakeCategoryCommand>>();
+
+                var command = new FakeCategoryCommand
+                {
+                    Name = name,
+                    Description = string.Empty
+                };
+
+
+                var result = await sut.ValidateAsync(command);
+
+                result.IsValid.Should().BeFalse();
+
+                result.Errors.Count().Should().Be(1);
+
+            }
+
+        }
+
+        [Test]
+        public async Task ShouldFailWhenCategoryDescriptionIsNotMetWithMinimumRequiredLenght()
+        {
+            using (var scope = ServiceProvider.CreateScope())
+            {
+                var sut = scope.ServiceProvider.GetRequiredService<IValidator<FakeCategoryCommand>>();
+
+                var command = new FakeCategoryCommand
+                {
+                    Name = Guid.NewGuid().ToString(),
+                    Description = "aa"
+                };
+
+
+                var result = await sut.ValidateAsync(command);
+
+                result.IsValid.Should().BeFalse();
+
+                result.Errors.Count().Should().Be(1);
+
+            }
+
+        }
+
+
+
+        private class FakeCategoryCommand : CategoryCommandBase
+        {
+
+        }
+
+
+        private class FakeCategoryCommandValidator : CategoryCommandValidatorBase<FakeCategoryCommand>
+        {
+
+        }
+    }
+}
---
+++ MicroStore.Catalog.Application.Tests/Categories/Validation/CreateCategoryCommandValidationTest.cs
@@ -0,0 +1,53 @@
+﻿using FluentAssertions;
+using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+using MicroStore.Catalog.Application.Abstractions.Categories.Commands;
+using MicroStore.Catalog.Domain.Entities;
+using Volo.Abp.Domain.Repositories;
+
+namespace MicroStore.Catalog.Application.Tests.Categories.Validation
+{
+    public class CreateCategoryCommandValidationTest : BaseTestFixture
+    {
+
+
+
+        [Test]
+        public async Task ShouldFailWhenCategoryNameIsExistInDatabase()
+        {
+            await WithUnitOfWork(async (sp) =>
+            {
+                var sut = (CreateCategoryCommandValidation)sp
+                .GetRequiredService<IValidator<CreateCategoryCommand>>();
+
+                var command = new CreateCategoryCommand { Name = "DublicateName" };
+
+                var result = await sut.ValidateAsync(command);
+
+                result.IsValid.Should().BeFalse();
+
+                result.Errors.Count.Should().Be(1);
+
+            });
+        }
+
+
+        [SetUp]
+        protected async Task SetupBeforeEachTest()
+        {
+            await CreateFakeCategory(new Category("DublicateName"));
+        }
+
+
+        private Task<Category> CreateFakeCategory(Category category)
+        {
+            return WithUnitOfWork(async (sp) =>
+            {
+                var repository = sp.GetRequiredService<IRepository<Category>>();
+                await repository.InsertAsync(category);
+                return category;
+            });
+        }
+
+    }
+}
