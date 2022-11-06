6e597b96ea84740dba8ef5d2cd3c24e759f86717
Mohamed Gamal <mohamedsherf17@gmail.com>
2022-11-07 01:26:14 +0200

    Add ShoppingCart Commands

---
+++ MicroStore.ShoppingCart.Application.Abstraction/Commands/RemoveBasketItemCommand.cs
@@ -0,0 +1,10 @@
+﻿using MicroStore.BuildingBlocks.InMemoryBus.Contracts;
+using MicroStore.ShoppingCart.Application.Abstraction.Dtos;
+namespace MicroStore.ShoppingCart.Application.Abstraction.Commands
+{
+    public class RemoveBasketItemCommand : ICommand<BasketDto>
+    {
+        public Guid BasketId { get; set; }
+        public Guid BasketItemId { get; set; }
+    }
+}
