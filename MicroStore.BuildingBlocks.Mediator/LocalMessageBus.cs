63f0c50dbb352775f40fc94054f3fbeef274e21c
Mohamed Gamal <mohamedsherf17@gmail.com>
2022-11-06 19:37:13 +0200

    Add MicroStore.BuildingBlocks.Mediator

---
+++ MicroStore.BuildingBlocks.Mediator/LocalMessageBus.cs
@@ -0,0 +1,20 @@
+﻿using MicroStore.BuildingBlocks.InMemoryBus.Contracts;
+
+namespace MicroStore.BuildingBlocks.Mediator
+{
+    internal class LocalMessageBus
+    {
+        private readonly MediatR.IMediator _mediator;
+
+        public LocalMessageBus(MediatR.IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        public Task<TResponse> Send<TResponse>(IRequest<TResponse> request, CancellationToken cancellationToken = default)
+
+        {
+            return _mediator.Send(new RequestAdapter<TResponse>(request), cancellationToken);
+        }
+    }
+}
