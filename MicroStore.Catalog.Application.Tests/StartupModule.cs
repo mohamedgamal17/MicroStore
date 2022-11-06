1e7dd352498c8793d45a3e19764fd236971b6bbd
Mohamed Gamal <mohamedsherf17@gmail.com>
2022-11-07 00:37:00 +0200

    Fix MicroStore.Catalog.Application.Tests

--- MicroStore.Catalog.Application.Tests/StartupModule.cs
+++ MicroStore.Catalog.Application.Tests/StartupModule.cs
@@ -1,12 +1,17 @@
-﻿
-
-using MassTransit;
+﻿using MassTransit;
+using MicroStore.BuildingBlocks.Mediator;
+using MicroStore.Catalog.Infrastructure;
 using System.Reflection;
 using Volo.Abp;
+using Volo.Abp.Autofac;
 using Volo.Abp.Modularity;
 
 namespace MicroStore.Catalog.Application.Tests
 {
+    [DependsOn(typeof(CatalogApplicationModule))]
+    [DependsOn(typeof(CatalogInfrastructureModule))]
+    [DependsOn(typeof(AbpAutofacModule))]
+    [DependsOn(typeof(MediatorModule))]
     public class StartupModule : AbpModule
     {
 
