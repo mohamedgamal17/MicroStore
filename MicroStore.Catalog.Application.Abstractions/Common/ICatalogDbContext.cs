557a8c6dd405bed7098dcc9b1430283d36b3f4a2
Mohamed Gamal <mohamedsherf17@gmail.com>
2022-11-06 20:36:07 +0200

    Add CatalogDbContext Interface

---
+++ MicroStore.Catalog.Application.Abstractions/Common/ICatalogDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MicroStore.Catalog.Domain.Entities;
+
+namespace MicroStore.Catalog.Application.Abstractions.Common
+{
+    public interface ICatalogDbContext
+    {
+
+        DbSet<Product> Products { get; set; }
+
+        DbSet<Category> Categories { get; set; }
+
+        DbSet<ProductCategory> ProductCategories { get; set; }
+    }
+}
