9693d76bbf4f641c74b36ecba67e87f72b1ae16a
Mohamed Gamal <mohamedsherf17@gmail.com>
2022-11-07 01:59:48 +0200

    Add MicroStore.Ordering.IntegrationEvents

---
+++ MicroStore.Ordering.IntegrationEvents/ConfirmOrderIntegrationEvent.cs
@@ -0,0 +1,9 @@
+﻿namespace MicroStore.Ordering.IntegrationEvents
+{
+    public class ConfirmOrderIntegrationEvent
+    {
+        public Guid OrderId { get; set; }
+        public string OrderNumber { get; set; }
+        public DateTime ConfirmationDate { get; set; }
+    }
+}
