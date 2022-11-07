5a26ad991aceebcdfdafbe99be3f14cddfcc640d
Mohamed Gamal <mohamedsherf17@gmail.com>
2022-11-07 23:29:59 +0200

    Add MicroStore.Shipping.IntegrationEvent

---
+++ MicroStore.Shipping.IntegrationEvent/ShippmentCreatedIntegrationEvent.cs
@@ -0,0 +1,9 @@
+﻿namespace MicroStore.Shipping.IntegrationEvent
+{
+    public class ShippmentCreatedIntegrationEvent
+    {
+        public Guid OrderId { get; set; }
+        public string OrderNumber { get; set; }
+        public string ShippmentId { get; set; }
+    }
+}
