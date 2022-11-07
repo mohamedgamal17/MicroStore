150a65a6155c4ec0125146256f46314dedfe6d9b
Mohamed Gamal <mohamedsherf17@gmail.com>
2022-11-07 20:12:10 +0200

    Remove IMapFrom Interface

--- MicroStore.ShoppingCart.Application.Abstraction/Dtos/BasketDto.cs
+++ MicroStore.ShoppingCart.Application.Abstraction/Dtos/BasketDto.cs
@@ -1,5 +1,4 @@
-﻿
-
+﻿#nullable disable
 namespace MicroStore.ShoppingCart.Application.Abstraction.Dtos
 {
     public class BasketDto 
@@ -9,13 +8,5 @@ namespace MicroStore.ShoppingCart.Application.Abstraction.Dtos
         public IEnumerable<BasketItemDto> LineItems { get; set; }
         public decimal SubTotal { get; set; }
 
-        public void Map(Profile profile)
-        {
-            profile.CreateMap<Basket, BasketDto>()
-                .ForMember(x => x.BasketId, opt => opt.MapFrom(c => c.Id))
-                .ForMember(x => x.UserId, opt => opt.MapFrom(c => c.UserId))
-                .ForMember(x => x.LineItems, opt => opt.MapFrom(c => c.LineItems));
-
-        }
     }
 }
--- MicroStore.ShoppingCart.Application.Abstraction/Dtos/BasketItemDto.cs
+++ MicroStore.ShoppingCart.Application.Abstraction/Dtos/BasketItemDto.cs
@@ -1,5 +1,4 @@
-﻿
-
+﻿#nullable disable
 namespace MicroStore.ShoppingCart.Application.Abstraction.Dtos
 {
     public class BasketItemDto 
@@ -7,13 +6,6 @@ namespace MicroStore.ShoppingCart.Application.Abstraction.Dtos
         public Guid ItemId { get; set; }
         public ProductDTO Product { get; set; }
         public int Quantity { get; set; }
-        public void Map(Profile profile)
-        {
-            profile.CreateMap<BasketItem, BasketItemDto>()
-                .ForMember(x => x.ItemId, opt => opt.MapFrom(c => c.Id))
-                .ForMember(x => x.Product, opt => opt.MapFrom(c => c.Product))
-                .ForMember(x => x.Quantity, opt => opt.MapFrom(c => c.Quantity));
-
-        }
+        
     }
 }
