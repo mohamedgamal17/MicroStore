783d23849255071ce6cb3f8b3044384692f201c0
Mohamed Gamal <mohamedsherf17@gmail.com>
2022-11-08 20:41:20 +0200

    Add PaymentCreatedResponse

---
+++ MicroStore.Payment.IntegrationEvents/Responses/PaymentCreatedResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace MicroStore.Payment.IntegrationEvents.Responses
+{
+    public class PaymentCreatedResponse
+    {
+        public string TransactionId { get; set; }
+        public string Gateway { get; set; }
+    }
+}
