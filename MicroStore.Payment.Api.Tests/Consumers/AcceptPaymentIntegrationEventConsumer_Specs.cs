e96af4731bfc8843559c35c2e6544e7f44fdc2b5
Mohamed Gamal <mohamedsherf17@gmail.com>
2022-11-09 20:49:17 +0200

    Add MicroStore.Payment.Api.Tests

---
+++ MicroStore.Payment.Api.Tests/Consumers/AcceptPaymentIntegrationEventConsumer_Specs.cs
@@ -0,0 +1,47 @@
+﻿using MicroStore.Payment.IntegrationEvents;
+using Moq;
+
+namespace MicroStore.Payment.Api.Tests.Consumers
+{
+    public class When_Accept_payment_integration_event_consumed : MassTransitTestFixture
+    {
+
+
+        [Test]
+        public async Task Should_capture_payment_and_publish_payment_accepted_integration_event()
+        {
+            await TestHarness.Bus.Publish(new AcceptPaymentIntegationEvent 
+            {
+                TransactionId = Guid.NewGuid().ToString() 
+            });
+
+
+            Assert.That(await TestHarness.Consumed.Any<AcceptPaymentIntegationEvent>());
+
+            Assert.That(await TestHarness.Published.Any<PaymentAccepetedIntegrationEvent>());
+
+            MockedPaymentService.Verify(c=> c.CapturePayment(It.IsAny<string>()),Times.Once());
+        }
+
+        [Test]
+        public async Task Should_refund_user_payment_and_publish_payment_rejected_when_any_error_happen()
+        {
+            MockedPaymentService.Setup(c => c.CapturePayment(It.IsAny<string>()))
+                .Callback(() => throw new InvalidOperationException());
+
+            await TestHarness.Bus.Publish(new AcceptPaymentIntegationEvent
+            {
+                TransactionId = Guid.NewGuid().ToString()
+            });
+
+            Assert.That(await TestHarness.Consumed.Any<AcceptPaymentIntegationEvent>());
+
+            Assert.That(await TestHarness.Published.Any<PaymentRejectedIntegrationEvent>());
+
+            MockedPaymentService.Verify(c => c.RefundPayment(It.IsAny<string>()), Times.Once);
+        }
+
+
+
+    }
+}
---
+++ MicroStore.Payment.Api.Tests/MassTransitTestFixture.cs
@@ -0,0 +1,55 @@
+﻿
+using Microsoft.Extensions.DependencyInjection;
+using MicroStore.Payment.Api.Dtos;
+using MicroStore.Payment.Api.Models;
+using MicroStore.Payment.Api.Services;
+using MicroStore.TestBase;
+using MicroStore.TestBase.Extensions;
+using Moq;
+
+namespace MicroStore.Payment.Api.Tests
+{
+    public class MassTransitTestFixture : MassTransitTestBase<PaymentApiTestModule>
+    {
+
+        public Mock<IPaymentService> MockedPaymentService { get; private set; }
+
+        public MassTransitTestFixture()
+        {
+            MockedPaymentService = new Mock<IPaymentService>();
+
+            MockedPaymentService.Setup(c => c.CreatePayment(It.IsAny<CreatePaymentModel>()))
+                .ReturnsAsync(new PaymentDto
+                {
+                    Amount = 5000,
+                    TransactionId = Guid.NewGuid().ToString()
+                });
+
+            MockedPaymentService.Setup(c => c.CapturePayment(It.IsAny<string>()));
+
+            MockedPaymentService.Setup(c => c.RefundPayment(It.IsAny<string>()));
+
+        }
+
+        [OneTimeSetUp]
+        public Task SetupBeforeAnyTest()
+        {
+            return StartMassTransit();
+        }
+
+
+        [OneTimeTearDown]
+        public Task SetupAfterAllTests()
+        {
+            return StartMassTransit();
+        }
+
+
+        protected override void AfterAddApplication(IServiceCollection services)
+        {
+            services.Remove<IPaymentService>();
+
+            services.AddTransient(typeof(IPaymentService), _ => MockedPaymentService.Object);
+        }
+    }
+}
