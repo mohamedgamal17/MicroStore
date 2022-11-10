fc43e3b2d12c3a483c127102af073cd23079c82f
Mohamed Gamal <mohamedsherf17@gmail.com>
2022-11-10 23:55:38 +0200

    Remove CurrentState File

--- MicroStore.Inventory.Application/ProductAggregate/CurrentState.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MicroStore.Inventory.Application.ProductAggregate
-{
-    public class CurrentState
-    {
-        public int StockOnHand { get; set; }
-        public int AllocatedStock { get; set; }
-
-    }
-}
