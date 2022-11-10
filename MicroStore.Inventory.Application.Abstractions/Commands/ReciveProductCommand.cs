d35d3bad289dc3647e9a5530f3e778778bc3e5f4
Mohamed Gamal <mohamedsherf17@gmail.com>
2022-11-10 02:18:42 +0200

    Add ReciveProductCommandValidator

--- MicroStore.Inventory.Application.Abstractions/Commands/ReciveProductCommand.cs
+++ MicroStore.Inventory.Application.Abstractions/Commands/ReciveProductCommand.cs
@@ -1,4 +1,5 @@
-﻿using MicroStore.BuildingBlocks.InMemoryBus.Contracts;
+﻿using FluentValidation;
+using MicroStore.BuildingBlocks.InMemoryBus.Contracts;
 
 namespace MicroStore.Inventory.Application.Abstractions.Commands
 {
@@ -7,5 +8,15 @@ namespace MicroStore.Inventory.Application.Abstractions.Commands
         public Guid ProductId { get; set; }
         public int Quantity { get; set; }
 
+    }
+
+    internal class ReciveProductCommandValidator : AbstractValidator<ReciveProductCommand>
+    {
+        public ReciveProductCommandValidator()
+        {
+            RuleFor(x => x.Quantity)
+                .GreaterThan(0)
+                .WithMessage("Product recived quantity cannot be zero or negative");
+        }
     }
 }
