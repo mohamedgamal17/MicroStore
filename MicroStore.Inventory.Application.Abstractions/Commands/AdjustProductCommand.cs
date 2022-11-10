5debe349fe0787f96268547b0db386dc408245b2
Mohamed Gamal <mohamedsherf17@gmail.com>
2022-11-10 02:14:09 +0200

    Add Product Commands

---
+++ MicroStore.Inventory.Application.Abstractions/Commands/AdjustProductCommand.cs
@@ -0,0 +1,10 @@
+﻿using MicroStore.BuildingBlocks.InMemoryBus.Contracts;
+
+namespace MicroStore.Inventory.Application.Abstractions.Commands
+{
+    public class AdjustProductCommand : ICommand
+    {
+        public Guid ProductId { get; set; }
+        public int Quantity { get; set; }
+    }
+}
---
+++ MicroStore.Inventory.Application.Abstractions/Commands/ReciveProductCommand.cs
@@ -0,0 +1,11 @@
+﻿using MicroStore.BuildingBlocks.InMemoryBus.Contracts;
+
+namespace MicroStore.Inventory.Application.Abstractions.Commands
+{
+    public class ReciveProductCommand : ICommand
+    {
+        public Guid ProductId { get; set; }
+        public int Quantity { get; set; }
+
+    }
+}
