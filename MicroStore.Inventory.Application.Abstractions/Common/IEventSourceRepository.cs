aa62276122638e954485a7cbbf00506314f23ccd
Mohamed Gamal <mohamedsherf17@gmail.com>
2022-11-10 02:13:18 +0200

    Add IEventSourceRepository

---
+++ MicroStore.Inventory.Application.Abstractions/Common/IEventSourceRepository.cs
@@ -0,0 +1,10 @@
+﻿
+namespace MicroStore.Inventory.Application.Abstractions.Common
+{
+    public interface IEventSourceRepository<TAggregate> where TAggregate : IAggregateRoot
+    {
+        Task<TAggregate?> Load(Guid aggregateId);
+
+        Task Save(TAggregate aggregateRoot);
+    }
+}
