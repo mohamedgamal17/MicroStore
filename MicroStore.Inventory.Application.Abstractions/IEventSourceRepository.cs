cbe18dd240eff3d5bf108d677279571be1fe1521
Mohamed Gamal <mohamedsherf17@gmail.com>
2022-11-10 23:56:04 +0200

    Add IEventSourceRepository Interface

---
+++ MicroStore.Inventory.Application.Abstractions/IEventSourceRepository.cs
@@ -0,0 +1,13 @@
+﻿
+using MicroStore.Inventory.Application.Abstractions.Common;
+
+namespace MicroStore.Inventory.Application.Abstractions
+{
+    public interface IEventSourceRepository<TAggregate> where TAggregate : class, IAggregateRoot
+    {
+        Task Load(TAggregate aggregateRoot);
+
+        Task Save(TAggregate aggregateRoot);
+
+    }
+}
