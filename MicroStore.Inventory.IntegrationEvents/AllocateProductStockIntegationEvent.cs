bf505d4f3cac55e171bde543cf400dad51270de6
Mohamed Gamal <mohamedsherf17@gmail.com>
2022-11-13 20:01:02 +0200

    remove AllocateProductStockIntegationEvent

--- MicroStore.Inventory.IntegrationEvents/AllocateProductStockIntegationEvent.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace MicroStore.Inventory.IntegrationEvents
-{
-    public class AllocateProductStockIntegationEvent
-    {
-        public Guid ProductId { get; set; }
-        public int Stock { get; set; }
-    }
-}
