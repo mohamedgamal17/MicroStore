818c2ef8506ef83fd6d9d2c20915a868e5b55466
Mohamed Gamal <mohamedsherf17@gmail.com>
2022-11-13 20:51:41 +0200

    Add ConsumerUnitOfWorkFilter

---
+++ MicroStore.Inventory.Application/ConsumerUnitOfWorkFilter.cs
@@ -0,0 +1,30 @@
+﻿using MassTransit;
+using Volo.Abp.Uow;
+
+namespace MicroStore.Inventory.Application
+{
+    public class ConsumerUnitOfWorkFilter<TMessage> : IFilter<ConsumeContext<TMessage>>
+       where TMessage : class
+    {
+        private readonly IUnitOfWorkManager _unitOfWorkManager;
+
+        public ConsumerUnitOfWorkFilter(IUnitOfWorkManager unitOfWorkManager)
+        {
+            _unitOfWorkManager = unitOfWorkManager;
+        }
+
+        public void Probe(ProbeContext context)
+        {
+            context.CreateScope("unit-of-work");
+        }
+
+        public async Task Send(ConsumeContext<TMessage> context, IPipe<ConsumeContext<TMessage>> next)
+        {
+            using var uow = _unitOfWorkManager.Begin(true);
+
+            await next.Send(context);
+
+            await uow.CompleteAsync();
+        }
+    }
+}
---
+++ MicroStore.Inventory.Application/InventoryApplicationModule.cs
@@ -0,0 +1,46 @@
+﻿using MassTransit;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MicroStore.Inventory.Application.Abstractions;
+using System.Reflection;
+using Volo.Abp.Modularity;
+
+namespace MicroStore.Inventory.Application
+{
+    [DependsOn(typeof(InventoryAbstractionModule))]
+    public class InventoryApplicationModule  : AbpModule
+    {
+
+        public override void ConfigureServices(ServiceConfigurationContext context)
+        {
+
+            ConfigureMassTranstit(context.Services, context.Services.GetConfiguration());
+
+        }
+
+
+        private void ConfigureMassTranstit(IServiceCollection services , IConfiguration configuration)
+        {
+            services.AddMassTransit(busRegisterConfig =>
+            {
+                busRegisterConfig.AddConsumers(Assembly.GetExecutingAssembly());
+
+             
+                busRegisterConfig.UsingRabbitMq((context, rabbitMqbusConig) =>
+                {
+                    rabbitMqbusConig.UseConsumeFilter(typeof(ConsumerUnitOfWorkFilter<>), context);
+
+                    rabbitMqbusConig.Host(configuration.GetValue<string>("MassTransitConfig:Host"), cfg =>
+                    {
+                        cfg.Username("MassTransitConfig:UserName");
+                        cfg.Password("MassTransitConfig:Password");
+                    });
+
+                    rabbitMqbusConig.ConfigureEndpoints(context);
+                });
+
+            });
+        }
+
+    }
+}
