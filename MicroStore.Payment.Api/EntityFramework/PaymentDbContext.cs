abe70d523ca0e9abd6282bf4ea177a6d10372c2d
Mohamed Gamal <mohamedsherf17@gmail.com>
2022-11-14 23:07:58 +0200

    add PaymentDbContext in payment service

---
+++ MicroStore.Payment.Api/EntityFramework/PaymentDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+using Volo.Abp.EntityFrameworkCore;
+
+namespace MicroStore.Payment.Api.EntityFramework
+{
+    public class PaymentDbContext : AbpDbContext<PaymentDbContext>
+    {
+        public PaymentDbContext(DbContextOptions<PaymentDbContext> options) 
+            : base(options)
+        {
+
+        }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
