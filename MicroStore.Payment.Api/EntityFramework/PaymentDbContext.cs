14261afff94423f2886390d80d9a9f25abe0f636
Mohamed Gamal <mohamedsherf17@gmail.com>
2022-11-14 23:10:11 +0200

    add PaymentDbContext

--- MicroStore.Payment.Api/EntityFramework/PaymentDbContext.cs
+++ MicroStore.Payment.Api/EntityFramework/PaymentDbContext.cs
@@ -1,11 +1,15 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿#nullable disable
+
+using Microsoft.EntityFrameworkCore;
+using MicroStore.Payment.Api.Domain;
 using System.Reflection;
 using Volo.Abp.EntityFrameworkCore;
-
 namespace MicroStore.Payment.Api.EntityFramework
 {
     public class PaymentDbContext : AbpDbContext<PaymentDbContext>
     {
+        public DbSet<PaymenRequest> Payments { get; set; }
+
         public PaymentDbContext(DbContextOptions<PaymentDbContext> options) 
             : base(options)
         {
