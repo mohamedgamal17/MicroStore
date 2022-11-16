84b32e6810a8f02b9da669813df37fef29865848
Mohamed Gamal <mohamedsherf17@gmail.com>
2022-11-17 01:37:40 +0200

    add payment request dtos

---
+++ MicroStore.Payment.Application/Dtos/PaymentCreatedDto.cs
@@ -0,0 +1,13 @@
+﻿#nullable disable
+namespace MicroStore.Payment.Application.Dtos
+{
+    public class PaymentCreatedDto
+    {
+        public Guid PaymentId { get; set; }
+        public Guid OrderId { get; set; }
+        public string OrderNumber { get; set; }
+        public string CustomerId { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
