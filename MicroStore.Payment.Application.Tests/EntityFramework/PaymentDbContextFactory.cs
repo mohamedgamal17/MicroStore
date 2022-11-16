0c469278d9e213c42798a6a72c6f2bad0376f187
Mohamed Gamal <mohamedsherf17@gmail.com>
2022-11-17 01:43:18 +0200

    add payment db context factory for test purpose

---
+++ MicroStore.Payment.Application.Tests/EntityFramework/PaymentDbContextFactory.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using MicroStore.Payment.Application.EntityFramework;
+
+namespace MicroStore.Payment.Application.Tests.EntityFramework
+{
+    internal class PaymentDbContextFactory : IDesignTimeDbContextFactory<PaymentDbContext>
+    {
+        public PaymentDbContext CreateDbContext(string[] args)
+        {
+            var config = BuildConfiguration();
+
+            var builder = new DbContextOptionsBuilder<PaymentDbContext>()
+                .UseSqlServer(config.GetConnectionString("DefaultConnection"), (opt) =>
+                {
+                    opt.UseQuerySplittingBehavior(QuerySplittingBehavior.SplitQuery);
+                    opt.MigrationsAssembly(typeof(PaymentDbContext).Assembly.FullName);
+                });
+
+
+            return new PaymentDbContext(builder.Options);
+        }
+        private IConfigurationRoot BuildConfiguration()
+        {
+            return new ConfigurationBuilder()
+                .SetBasePath(Path.Combine(Directory.GetCurrentDirectory(), "../MicroStore.Catalog.Application.Tests/"))
+                .AddJsonFile("appsettings.json", false)
+                .Build();
+        }
+    }
+}
