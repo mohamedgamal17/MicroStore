489be9d0a2ee5d9e7215f276c19a07d46f565f01
Mohamed Gamal <mohamedsherf17@gmail.com>
2022-11-17 01:42:02 +0200

    add payment host application

---
+++ MicroStore.Payment.Api.Host/PaymentApiHostModule.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.OpenApi.Models;
+using MicroStore.Payment.Plugin.StripeGateway;
+using Volo.Abp;
+using Volo.Abp.AspNetCore.ExceptionHandling;
+using Volo.Abp.AspNetCore.Mvc.AntiForgery;
+using Volo.Abp.AspNetCore.Serilog;
+using Volo.Abp.Autofac;
+using Volo.Abp.Modularity;
+
+namespace MicroStore.Payment.Api.Host
+{
+
+    [DependsOn(typeof(PaymentApiModule),
+        typeof(StripeGatewayPluginModule),
+        typeof(AbpAutofacModule),
+    typeof(AbpAspNetCoreSerilogModule))]
+    public class PaymentApiHostModule : AbpModule
+    {
+        public override void ConfigureServices(ServiceConfigurationContext context)
+        {
+            var host = context.Services.GetHostingEnvironment();
+
+            var configuration = context.Services.GetConfiguration();
+
+            ConfigureAuthentication(context.Services);
+            ConfigureSwagger(context.Services);
+
+            Configure<AbpExceptionHandlingOptions>(options =>
+            {
+                options.SendExceptionsDetailsToClients = false;
+                options.SendStackTraceToClients = false;
+            });
+
+            Configure<AbpAntiForgeryOptions>(opt =>
+            {
+                opt.AutoValidate = false;
+            });
+
+        }
+
+        private void ConfigureAuthentication(IServiceCollection services)
+        {
+
+
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, opt =>
+                {
+                    opt.Authority = "https://localhost:5001/";
+                    opt.Audience = "CatalogApi";
+
+                });
+        }
+
+        public override void OnApplicationInitialization(ApplicationInitializationContext context)
+        {
+            var app = context.GetApplicationBuilder();
+            var env = context.GetEnvironment();
+
+            if (env.IsDevelopment())
+            {
+
+                app.UseSwagger();
+                app.UseSwaggerUI(options =>
+                {
+                    options.SwaggerEndpoint("/swagger/v1/swagger.json", "Catalog API");
+                });
+
+
+                app.UseHsts();
+
+            }
+
+
+            app.UseAbpRequestLocalization();
+
+            app.UseCorrelationId();
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.UseAuthentication();
+            app.UseAuthorization();
+            app.UseAbpSerilogEnrichers();
+            app.UseConfiguredEndpoints();
+
+            //app.MapControllers();
+        }
+
+
+
+
+
+        private void ConfigureSwagger(IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddSwaggerGen((options) =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo { Title = "Catalog Api", Version = "v1" });
+                options.DocInclusionPredicate((docName, description) => true);
+                options.CustomSchemaIds(type => type.FullName);
+            });
+        }
+
+    }
+}
---
+++ MicroStore.Payment.Api.Host/Program.cs
@@ -0,0 +1,40 @@
+using MicroStore.Payment.Api.Host;
+using Serilog;
+using Serilog.Events;
+
+
+
+Log.Logger = new LoggerConfiguration()
+#if DEBUG
+    .MinimumLevel.Debug()
+#else
+    .MinimumLevel.Information()
+#endif
+    .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+    .MinimumLevel.Override("Microsoft.EntityFrameworkCore", LogEventLevel.Warning)
+    .Enrich.FromLogContext()
+    .WriteTo.Async(c => c.File("Logs/logs.txt"))
+    .WriteTo.Async(c => c.Console())
+    .CreateLogger();
+
+try
+{
+    Log.Information("Starting web host.");
+    var builder = WebApplication.CreateBuilder(args);
+    builder.Host.AddAppSettingsSecretsJson()
+        .UseAutofac()
+        .UseSerilog();
+    await builder.AddApplicationAsync<PaymentApiHostModule>();
+
+    var app = builder.Build();
+    await app.InitializeApplicationAsync();
+    await app.RunAsync();
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Host terminated unexpectedly!");
+}
+finally
+{
+    Log.CloseAndFlush(); ;
+}
