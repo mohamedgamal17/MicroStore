5188f81b031aee1b0af882e6e4c1a4a02a34a25f
Mohamed Gamal <mohamedsherf17@gmail.com>
2022-11-17 01:42:28 +0200

    add payment commands tests

---
+++ MicroStore.Payment.Application.Tests/Commands/VoidPaymentRequestCommandHandler_Specs.cs
@@ -0,0 +1,28 @@
+﻿using FluentAssertions;
+using MicroStore.Payment.Application.Commands.Requests;
+using MicroStore.Payment.Application.Domain;
+
+namespace MicroStore.Payment.Application.Tests.Commands
+{
+    public class When_reciving_void_payment_command : BaseTestFixture
+    {
+
+        [Test]
+        public async Task Should_void_pament_request()
+        {
+
+            PaymentRequest fakepaymentRequest = await Insert(new PaymentRequest(Guid.NewGuid(), "fakeordernumber", "fakecustomerId", 50));
+
+            await Send(new VoidPaymentCommand
+            {
+                PaymentId = fakepaymentRequest.Id,
+                FaultDate = DateTime.UtcNow
+            });
+          
+            PaymentRequest paymentRequest = await Find<PaymentRequest>(x => x.Id == fakepaymentRequest.Id);
+
+            paymentRequest.State.Should().Be(PaymentStatus.Void);
+        }
+
+    }
+}
