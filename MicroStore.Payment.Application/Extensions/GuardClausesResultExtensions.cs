0b77f3d32d883e2db3886050da67da97948b4508
Mohamed Gamal <mohamedsherf17@gmail.com>
2022-11-17 01:38:00 +0200

    add guard clause extensions

---
+++ MicroStore.Payment.Application/Extensions/GuardClausesResultExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Ardalis.GuardClauses;
+using MicroStore.BuildingBlocks.Results;
+
+namespace MicroStore.Payment.Application.Extensions
+{
+    public static class GuardClausesResultExtensions
+    {
+
+        public static Result InvalidResult(this IGuardClause guardClause, Result result)
+        {
+            if (result.IsFailure)
+            {
+                throw new InvalidOperationException(result.Error);
+            }
+
+            return result;
+        }
+    }
+}
