cd278042818f6d0519640cdd486999da03d34b9f
Mohamed Gamal <mohamedsherf17@gmail.com>
2022-11-17 01:40:20 +0200

    add payment integation event consumers

---
+++ MicroStore.Payment.Application/Consumers/CreatePaymentIntegrationEventConsumer.cs
@@ -0,0 +1,48 @@
+﻿using MassTransit;
+using Microsoft.Extensions.Logging;
+using MicroStore.BuildingBlocks.InMemoryBus.Contracts;
+using MicroStore.Payment.Application.Commands.Requests;
+using MicroStore.Payment.Application.Domain;
+using MicroStore.Payment.IntegrationEvents;
+using Volo.Abp.Domain.Repositories;
+
+namespace MicroStore.Payment.Application.Consumers
+{
+    public class CreatePaymentIntegrationEventConsumer : IConsumer<CreatePaymentIntegrationEvent>
+    {
+
+        private ILocalMessageBus _localMessageBus;
+
+
+        public CreatePaymentIntegrationEventConsumer(ILocalMessageBus localMessageBus)
+        {
+            _localMessageBus = localMessageBus;
+        }
+
+   
+        public async Task Consume(ConsumeContext<CreatePaymentIntegrationEvent> context)
+        {
+
+            CreatePaymentRequestCommand createPaymentCommand = new CreatePaymentRequestCommand
+            {
+                OrderId = context.Message.OrderId,
+                OrderNumber = context.Message.OrderNumber,
+                Amount = context.Message.TotalPrice,
+                CustomerId = context.Message.CustomerId
+            };
+
+            var result = await _localMessageBus.Send(createPaymentCommand);
+
+            PaymentCreatedIntegrationEvent paymentCreatedIntegrationEvent = new PaymentCreatedIntegrationEvent
+            {
+                PaymentId = result.PaymentId.ToString(),
+                CustomerId = result.CustomerId,
+                OrderId = result.OrderId,
+                OrderNubmer = result.OrderNumber,         
+            };
+
+            await context.Publish(paymentCreatedIntegrationEvent);
+        }
+
+    }
+}
