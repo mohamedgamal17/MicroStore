38532d46a4d889cb7c8f0816e46dbe1a693c2380
Mohamed Gamal <mohamedsherf17@gmail.com>
2022-11-17 01:38:17 +0200

    add payment dbcontext

---
+++ MicroStore.Payment.Application/EntityFramework/EntityTypeConfigurations/PaymentRequestEntityTypeConfiguration.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MicroStore.Payment.Application.Domain;
+
+namespace MicroStore.Payment.Application.EntityFramework.EntityTypeConfigurations
+{
+    public class PaymentRequestEntityTypeConfiguration : IEntityTypeConfiguration<PaymentRequest>
+    {
+        public void Configure(EntityTypeBuilder<PaymentRequest> builder)
+        {
+
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.OrderId).IsRequired();
+
+            builder.Property(x => x.OrderNumber).HasMaxLength(265).IsRequired();
+
+            builder.Property(x => x.TransctionId).HasMaxLength(265);
+
+            builder.Property(x => x.Amount).IsRequired();
+
+            builder.Property(x => x.FaultAt);
+
+            builder.Property(x => x.FaultReason).HasMaxLength(265);
+
+            builder.HasIndex(x => x.OrderId).IsUnique();
+
+            builder.HasIndex(x => x.OrderNumber).IsUnique();
+
+            builder.HasIndex(x => x.TransctionId);
+
+        }
+    }
+}
---
+++ MicroStore.Payment.Application/EntityFramework/PaymentDbContext.cs
@@ -0,0 +1,30 @@
+﻿#nullable disable
+using Microsoft.EntityFrameworkCore;
+using MicroStore.Payment.Application.Common;
+using MicroStore.Payment.Application.Domain;
+using System.Reflection;
+using Volo.Abp.Data;
+using Volo.Abp.EntityFrameworkCore;
+namespace MicroStore.Payment.Application.EntityFramework
+{
+
+    [ConnectionStringName("DefaultConnection")]
+    public class PaymentDbContext : AbpDbContext<PaymentDbContext> , IPaymentDbContext
+    {
+        public DbSet<PaymentRequest> PaymentRequests { get; set; }
+
+        public PaymentDbContext(DbContextOptions<PaymentDbContext> options)
+            : base(options)
+        {
+
+        }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
