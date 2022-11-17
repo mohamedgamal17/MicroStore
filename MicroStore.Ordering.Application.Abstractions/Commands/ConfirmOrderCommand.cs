82bc1b3ff4b2c0bfa75ffe934e339c849f92c03b
Mohamed Gamal <mohamedsherf17@gmail.com>
2022-11-18 00:18:30 +0200

    Remove order application abstractions

--- MicroStore.Ordering.Application.Abstractions/Commands/ConfirmOrderCommand.cs
+++
@@ -1,8 +0,0 @@
-﻿using MicroStore.BuildingBlocks.InMemoryBus.Contracts;
-namespace MicroStore.Ordering.Application.Abstractions.Commands
-{
-    public class ConfirmOrderCommand : ICommand
-    {
-        public Guid OrderId { get; set; }
-    }
-}
