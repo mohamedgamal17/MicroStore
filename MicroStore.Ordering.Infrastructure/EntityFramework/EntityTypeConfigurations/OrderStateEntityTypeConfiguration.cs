bd8d910b0dbdaa62973d4cbc51db09ef04dc5124
Mohamed Gamal <mohamedsherf17@gmail.com>
2022-11-18 00:19:28 +0200

    Refactor order infrastructure

--- MicroStore.Ordering.Infrastructure/EntityFramework/EntityTypeConfigurations/OrderStateEntityTypeConfiguration.cs
+++ MicroStore.Ordering.Infrastructure/EntityFramework/EntityTypeConfigurations/OrderStateEntityTypeConfiguration.cs
@@ -12,25 +12,18 @@ namespace MicroStore.Ordering.Infrastructure.EntityFramework.EntityTypeConfigura
         {
             entity.Property(x => x.UserId).HasMaxLength(256);
             entity.Property(x => x.OrderNumber).HasMaxLength(265);
-            entity.Property(x => x.TransactionId).IsRequired(false).HasMaxLength(256);
+            entity.Property(x => x.PaymentId).IsRequired(false).HasMaxLength(256);
             entity.Property(x => x.ShippmentId).HasMaxLength(256);
             entity.Property(x => x.CancelledBy).HasMaxLength(256);
-            entity.Property(x => x.RejectedBy).HasMaxLength(256);
-            entity.Property(x => x.RejectionReason).HasMaxLength(500);
             entity.Property(x => x.FaultReason).HasMaxLength(500);
             entity.Property(x => x.CancellationReason).HasMaxLength(500);
-
             entity.HasMany(x => x.OrderItems).WithOne();
-
             entity.HasIndex(x => x.UserId);
             entity.HasIndex(x => x.OrderNumber).IsUnique();
             entity.HasIndex(x => x.ShippingAddressId);
             entity.HasIndex(x => x.BillingAddressId);
             entity.HasIndex(x => x.ShippmentId);
             entity.HasIndex(x => x.CancelledBy);
-            entity.HasIndex(x => x.RejectedBy);
-
-
             entity.Navigation(x => x.OrderItems).AutoInclude();
         }
     }
---
+++ MicroStore.Ordering.Infrastructure/Migrations/20221117011726_RefactorOrderStateMigration.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MicroStore.Ordering.Infrastructure.Migrations
+{
+    public partial class RefactorOrderStateMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_OrderStateEntity_RejectedBy",
+                table: "OrderStateEntity");
+
+            migrationBuilder.DropColumn(
+                name: "AcceptenceDate",
+                table: "OrderStateEntity");
+
+            migrationBuilder.DropColumn(
+                name: "RejectedBy",
+                table: "OrderStateEntity");
+
+            migrationBuilder.DropColumn(
+                name: "RejectionReason",
+                table: "OrderStateEntity");
+
+            migrationBuilder.RenameColumn(
+                name: "TransactionId",
+                table: "OrderStateEntity",
+                newName: "PaymentId");
+
+            migrationBuilder.RenameColumn(
+                name: "RejectionDate",
+                table: "OrderStateEntity",
+                newName: "PaymentAcceptedDate");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "PaymentId",
+                table: "OrderStateEntity",
+                newName: "TransactionId");
+
+            migrationBuilder.RenameColumn(
+                name: "PaymentAcceptedDate",
+                table: "OrderStateEntity",
+                newName: "RejectionDate");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "AcceptenceDate",
+                table: "OrderStateEntity",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "RejectedBy",
+                table: "OrderStateEntity",
+                type: "nvarchar(256)",
+                maxLength: 256,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "RejectionReason",
+                table: "OrderStateEntity",
+                type: "nvarchar(500)",
+                maxLength: 500,
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderStateEntity_RejectedBy",
+                table: "OrderStateEntity",
+                column: "RejectedBy");
+        }
+    }
+}
