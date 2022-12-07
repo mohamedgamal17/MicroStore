c03d9198276249596f7ba95518df23b32aaf7b33
Mohamed Gamal <mohamedsheref17@outlook.com>
2022-12-08 00:48:56 +0200

    Add Dimension ValueObject In Domain Of Catalog

---
+++ MicroStore.Catalog.Domain/ValueObjects/Dimension.cs
@@ -0,0 +1,47 @@
+﻿using MicroStore.Catalog.Domain.Common;
+
+namespace MicroStore.Catalog.Domain.ValueObjects
+{
+    public class Dimension : ValueObject<Dimension>
+    {
+        private readonly double _value;
+
+        private readonly string _unit;
+        public double Value => _value;
+        public string Unit => _unit;
+
+        private Dimension(double value, string unit)
+        {
+            _value = value;
+            _unit = unit;
+        }
+
+        private Dimension() { }
+      
+        protected override IEnumerable<object> GetEqualityComponents()
+        {
+            yield return _value;
+            yield return _unit;
+        }
+
+        public static Dimension FromCentiMeter(double value)
+        {
+            return new Dimension(value, DimensionUnit.CentiMeter);
+        }
+
+        public static Dimension FromMeter(double value)
+        {
+            return new Dimension(value, DimensionUnit.Meter);
+        }
+
+        public static Dimension FromInc(double value)
+        {
+            return new Dimension(value, DimensionUnit.Inch);
+        }
+
+        public static Dimension FromFeet(double value)
+        {
+            return new Dimension(value, DimensionUnit.Feet);
+        }
+    }
+}
---
+++ MicroStore.Catalog.Domain/ValueObjects/DimensionUnit.cs
@@ -0,0 +1,16 @@
+﻿namespace MicroStore.Catalog.Domain.ValueObjects
+{
+    public static class DimensionUnit
+    {
+        public static readonly string CentiMeter = "cm";
+
+        public static readonly string Meter = "m";
+
+        public static readonly string Inch = "inch";
+
+        public static readonly string Feet = "ft";
+
+        public static readonly string None = "none";
+
+    }
+}
