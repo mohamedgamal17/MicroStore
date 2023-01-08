49da399738cdacca098014a1c1008f74d05904ac
Mohamed Gamal <mohamedsheref17@outlook.com>
2023-01-08 02:39:03 +0200

    register fluent validation module in order application abstractions

--- MicroStore.Ordering.Application.Abstractions/OrderApplicationAbstractionModule.cs
+++ MicroStore.Ordering.Application.Abstractions/OrderApplicationAbstractionModule.cs
@@ -1,12 +1,14 @@
 ﻿using MicroStore.BuildingBlocks.InMemoryBus;
 using System.Reflection;
 using Volo.Abp.AutoMapper;
+using Volo.Abp.FluentValidation;
 using Volo.Abp.Modularity;
 
 namespace MicroStore.Ordering.Application.Abstractions
 {
     [DependsOn(typeof(AbpAutoMapperModule),
-        typeof(InMemoryBusModule))]
+        typeof(InMemoryBusModule),
+        typeof(AbpFluentValidationModule))]
     public class OrderApplicationAbstractionModule : AbpModule
     {
         public override void ConfigureServices(ServiceConfigurationContext context)
