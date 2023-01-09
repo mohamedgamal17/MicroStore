ab6b6c7623202fac431289cb7c00663aab6ce655
Mohamed Gamal <mohamedsheref17@outlook.com>
2023-01-09 03:50:24 +0200

    design inventory initial access control

---
+++ MicroStore.Inventory.Domain/Security/InventoryScope.cs
@@ -0,0 +1,23 @@
+﻿namespace MicroStore.Inventory.Domain.Security
+{
+    public static class InventoryScope
+    {
+
+        public static class Product
+        {
+            public static readonly string List = "inventory.product.list";
+
+            public static readonly string Read = "inventory.product.read";
+
+            public static readonly string AdjustQuantity = "inventory.product.adjustquantity";
+        }
+
+        public static class Order
+        {
+            public static readonly string List = "inventory.order.list";
+
+            public static readonly string Read = "inventory.order.read";
+
+        }
+    }
+}
