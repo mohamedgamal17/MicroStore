40da5e16d1279a13874ae359717eafed7892ee9b
Mohamed Gamal <mohamedsheref17@outlook.com>
2023-01-09 03:50:05 +0200

    design basket intial access control

---
+++ MicroStore.ShoppingCart.Api/Security/BasketScope.cs
@@ -0,0 +1,12 @@
+﻿namespace MicroStore.ShoppingCart.Api.Security
+{
+    public static class BasketScope
+    {
+
+        public static readonly string Read = "basket.read";
+
+        public static readonly string Update = "basket.update";
+
+        public static readonly string Migrate = "baske.migrate";
+    }
+}
