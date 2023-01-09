e377b2e62c8f6bc82e5452846fcb26eed69318c2
Mohamed Gamal <mohamedsheref17@outlook.com>
2023-01-09 03:50:36 +0200

    design ordering initial access control

---
+++ MicroStore.Ordering.Application.Abstractions/Security/OrderingScope.cs
@@ -0,0 +1,21 @@
+﻿namespace MicroStore.Ordering.Application.Abstractions.Security
+{
+    public static class OrderingScope
+    {
+
+        public static class Order
+        {
+            public static readonly string List = "ordering.order.list";
+
+            public static readonly string Read = "ordering.order.read";
+
+            public static readonly string Submit = "ordering.order.submit";
+
+            public static readonly string Fullfill = "ordering.order.fullfill";
+
+            public static readonly string Complete = "ordering.order.complete";
+
+            public static readonly string Cancel = "ordering.order.cancel";
+        }
+    }
+}
