c66259846842adf38e3eab71a2cbf27ec28d31d0
Mohamed Gamal <mohamedsheref17@outlook.com>
2023-06-25 16:32:15 +0300

    Fix ui order managment

---
+++ MicroStore.Client.PublicWeb/wwwroot/js/BackEnd/Order/details.js
@@ -0,0 +1,21 @@
+﻿$(document).ready(function () {
+    var orderId = $("#Id").val();
+
+    var cancelOrderModal = new abp.ModalManager({
+        viewUrl : "/Administration/Order/CancelOrderModal" 
+    })
+
+    $("#cancelOrderButton").click(function () {
+        cancelOrderModal.open({
+            id : orderId
+        })
+    })
+
+    $("#completeOrderButton").click(function () {
+        $("#completeOrderForm").submit()
+    })
+
+    cancelOrderModal.onResult(function () {
+        location.reload()
+    })
+})
