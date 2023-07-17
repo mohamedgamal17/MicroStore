6da18fd78964f92eb41aef57f506582489791612
Mohamed Gamal <mohamedsheref17@outlook.com>
2023-07-18 02:59:20 +0300

    Add category widget component

---
+++ MicroStore.Client.PublicWeb/Components/CategoryWidget/CategoryWidgetViewComponent.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MicroStore.ShoppingGateway.ClinetSdk.Entities.Catalog;
+using MicroStore.ShoppingGateway.ClinetSdk.Services.Catalog;
+using Volo.Abp.AspNetCore.Mvc;
+using Volo.Abp.AspNetCore.Mvc.UI.Widgets;
+namespace MicroStore.Client.PublicWeb.Components.CategoryWidget
+{
+    [Widget(AutoInitialize = true)]
+    public class CategoryWidgetViewComponent  : AbpViewComponent
+    {
+        private readonly CategoryService _categoryService;
+
+        public CategoryWidgetViewComponent(CategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var requestOptions = new CategoryListRequestOptions();
+
+            var categories = await _categoryService.ListAsync(requestOptions);
+
+
+            var model = new CategoryWidgetModel
+            {
+                Categories = categories
+            };
+
+            return View(model);
+        }
+    }
+
+    public class CategoryWidgetModel
+    {
+        public List<Category> Categories { get; set; }
+    }
+}
