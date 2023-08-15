7eb3316926e5ab639c417a27e1d088ee5dab279a
mohamedgamal17 <mohamedsherf17@gmail.com>
2023-08-15 09:43:46 +0300

    Define image search engine domain

---
+++ MicroStore.Catalog.Domain/Entities/ImageVector.cs
@@ -0,0 +1,10 @@
+﻿namespace MicroStore.Catalog.Domain.Entities
+{
+    public class ImageVector
+    {
+        public string Id { get; set; }
+        public string ProductId { get; set; }
+        public string ImageId { get; set; }
+        public List<float> Features { get; set; }
+    }
+}
