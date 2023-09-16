d5195dc8be772dc91520f44f94d9a1e587d36529
Mohamed Gamal <mohamedsheref17@outlook.com>
2023-09-16 15:37:58 +0300

    Add ClientSDK ProductReview Entity

---
+++ MicroStore.ShoppingGateway.ClinetSdk/Entities/Catalog/ProductRevivew.cs
@@ -0,0 +1,17 @@
+﻿namespace MicroStore.ShoppingGateway.ClinetSdk.Entities.Catalog
+{
+    public class ProductRevivew : BaseEntity<string>
+    {
+        public string UserId { get; set; }
+
+        public string ProductId { get; set; }
+
+        public string Title { get; set; }
+
+        public string ReviewText { get; set; }
+
+        public int Rating { get; set; }
+
+        public string ReplayText { get; set; }
+    }
+}
