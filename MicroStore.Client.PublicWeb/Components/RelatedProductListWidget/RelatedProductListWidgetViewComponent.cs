511646537d88f19bea9d4029e30be307bb66ddbd
mohamedgamal17 <mohamedsherf17@gmail.com>
2023-09-17 09:21:47 +0300

    Implement RelatedProductListWidget

---
+++ MicroStore.Client.PublicWeb/Components/RelatedProductListWidget/RelatedProductListWidgetViewComponent.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MicroStore.ShoppingGateway.ClinetSdk.Entities.Catalog;
+using MicroStore.ShoppingGateway.ClinetSdk.Services;
+using MicroStore.ShoppingGateway.ClinetSdk.Services.Catalog;
+using Volo.Abp.AspNetCore.Mvc;
+using Volo.Abp.AspNetCore.Mvc.UI.Widgets;
+
+namespace MicroStore.Client.PublicWeb.Components.RelatedProductListWidget
+{
+    [Widget(AutoInitialize = true,
+        ScriptFiles = new string[] { "/Pages/Shared/Components/RelatedProductListWidget/related-product-list-widget.js" })]
+    public class RelatedProductListWidgetViewComponent : AbpViewComponent
+    {
+        private readonly ProductRecommendationService _productRecommendationService;
+
+        public RelatedProductListWidgetViewComponent(ProductRecommendationService productRecommendationService)
+        {
+            _productRecommendationService = productRecommendationService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(string productId,int length = 6)
+        {
+            var pagingOptions = new PagingReqeustOptions
+            {
+                Length = length,
+                Skip = 0
+            };
+
+            var response =  await _productRecommendationService.GetSimilarItems(productId, pagingOptions);
+
+            var model = new RelatedProductListWidgetModel
+            {
+                Products = response.Items
+            };
+
+            return View(model);
+        }
+
+    }
+
+    public class RelatedProductListWidgetModel
+    {
+        public List<Product> Products { get; set; } = new List<Product>();
+    }
+}
