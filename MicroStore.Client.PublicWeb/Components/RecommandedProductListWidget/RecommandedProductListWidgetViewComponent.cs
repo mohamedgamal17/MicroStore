f82d59bfbccb390ff39fed84983dcd1a40855233
mohamedgamal17 <mohamedsherf17@gmail.com>
2023-09-20 01:01:03 +0300

    Implement Products Recommandation Widget

---
+++ MicroStore.Client.PublicWeb/Components/RecommandedProductListWidget/RecommandedProductListWidgetViewComponent.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MicroStore.ShoppingGateway.ClinetSdk.Entities.Catalog;
+using MicroStore.ShoppingGateway.ClinetSdk.Services;
+using MicroStore.ShoppingGateway.ClinetSdk.Services.Catalog;
+using Volo.Abp.AspNetCore.Mvc;
+using Volo.Abp.AspNetCore.Mvc.UI.Widgets;
+
+namespace MicroStore.Client.PublicWeb.Components.RecommandedProductListWidget
+{
+    [Widget(AutoInitialize = true,
+        ScriptFiles = new string[] { "/Pages/Shared/Components/RecommandedProductListWidget/recommanded-product-list-widget.js" })]
+    public class RecommandedProductListWidgetViewComponent : AbpViewComponent
+    {
+
+        private readonly ProductRecommendationService _productRecommendationService;
+
+        public RecommandedProductListWidgetViewComponent(ProductRecommendationService productRecommendationService)
+        {
+            _productRecommendationService = productRecommendationService;
+        }
+
+
+        public  async Task<IViewComponentResult> InvokeAsync()
+        {
+            bool isUserAuthenticated = HttpContext.User.Identity?.IsAuthenticated ?? false;
+
+            if (isUserAuthenticated)
+            {
+                var requestOptions = new PagingReqeustOptions
+                {
+                    Length = 10
+                };
+
+
+                var response =  await _productRecommendationService.GetProductRecommendation(requestOptions);
+
+                var model = new RecommandedProductListWidgetViewComponentModel
+                {
+                    Products = response.Items
+                };
+
+                return View(model);
+            }
+            else
+            {
+                var model = new RecommandedProductListWidgetViewComponentModel()
+                {
+                    Products = new List<Product>()
+                };
+
+                return View(model);
+            }
+        }
+    }
+
+
+
+    public class RecommandedProductListWidgetViewComponentModel
+    {
+        public List<Product> Products { get; set; }
+    }
+}
---
+++ MicroStore.Client.PublicWeb/Pages/Shared/Components/RecommandedProductListWidget/recommanded-product-list-widget.js
@@ -0,0 +1,34 @@
+﻿(function () {
+    abp.widgets.RelatedProductListWidget = function ($wrapper) {
+        var widgetManager = $wrapper.data("abp-widget-manager");
+        var init = function ($filters) {
+            $wrapper
+                .find(".add-to-cart")
+                .click(function () {
+                    var $this = $(this);
+                    var productId = $this.parents(".product-box").attr("data-product-id");
+                    console.log(productId);
+                    abp.ajax({
+                        url: "/api/basket",
+                        method: "POST",
+                        data: JSON.stringify({
+                            productId: productId,
+                            quantity: 1
+                        }),
+                        success: function () {
+                            $('.abp-widget-wrapper[data-widget-name="CartWidget"]')
+                                .data("abp-widget-manager")
+                                .refresh();
+
+                            abp.notify.info("Added product to your basket.", "Successfully added")
+                        }
+
+                    })
+                });
+        }
+
+        return {
+            init: init
+        }
+    }
+})();
