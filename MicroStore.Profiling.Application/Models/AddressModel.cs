9d761fb28d725593c93647dc3b29e9048782a20c
mohamedgamal17 <mohamedsherf17@gmail.com>
2023-09-20 05:53:16 +0300

    Add Model Validators

--- MicroStore.Profiling.Application/Models/AddressModel.cs
+++ MicroStore.Profiling.Application/Models/AddressModel.cs
@@ -1,4 +1,7 @@
-﻿namespace MicroStore.Profiling.Application.Models
+﻿using FluentValidation;
+using PhoneNumbers;
+
+namespace MicroStore.Profiling.Application.Models
 {
     public class AddressModel
     {
@@ -11,5 +14,94 @@
         public string Zip { get; set; }
         public string AddressLine1 { get; set; }
         public string AddressLine2 { get; set; }
+    }
+
+    internal class AddressValidator : AbstractValidator<AddressModel>
+    {
+        public AddressValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotNull()
+                .WithMessage("Address Name is required")
+                .MaximumLength(400)
+                .WithMessage("Address name maximum length is 400");
+
+            RuleFor(x => x.Phone)
+                 .NotNull()
+                 .WithMessage("Phone is required.")
+                 .MinimumLength(10)
+                 .WithMessage("Phone must not be less than 10 characters.")
+                 .MaximumLength(50)
+                 .WithMessage("Phone must not exceed 50 characters.")
+                 .Must(ValidatePhone)
+                 .WithMessage("Invalid phone number");
+
+            RuleFor(x => x.CountryCode)
+                .NotNull()
+                .WithMessage("Country Code is required")
+                .MinimumLength(2)
+                .WithMessage("Country Code minimum lenght is 2")
+                .MaximumLength(3)
+                .WithMessage("Country Code maximum length is 3");
+
+            RuleFor(x => x.State)
+                .NotNull()
+                .WithMessage("State is required")
+                .Length(2)
+                .WithMessage("State code exact lenght is 2");
+
+
+            RuleFor(x => x.City)
+                .NotNull()
+                .WithMessage("City is required")
+                .Matches(@"[a-zA-Z\u00C0-\u01FF- ]+")
+                .WithMessage("Please enter only letters and spaces, no numbers or special characters.")
+                .Length(2, 64)
+                .WithMessage("State names should be between 2 and 64 characters long.");
+
+
+            RuleFor(x => x.PostalCode)
+             .NotEmpty()
+             .WithMessage("Postal Code is required")
+             .Length(3, 10)
+             .WithMessage("Postal code must be between 3 and 10 digits long, and valid for the country of residence.");
+
+            RuleFor(x => x.Zip)
+             .NotEmpty()
+             .WithMessage("Please enter a valid zip code.")
+             .Length(5, 10)
+             .WithMessage("Zip Code should be between 5 and 10 characters long.");
+
+            RuleFor(x => x.AddressLine1)
+              .NotEmpty()
+              .WithMessage("Address Line 1 is required")
+              .Length(6, 128)
+              .WithMessage("Addresses should be between 6 and 128 characters long.");
+
+
+            RuleFor(x => x.AddressLine1)
+              .Length(6, 128)
+              .WithMessage("Addresses should be between 6 and 128 characters long.");
+
+        }
+
+
+        private bool ValidatePhone(AddressModel model, string phone)
+        {
+            var phoneNumberUtil = PhoneNumberUtil.GetInstance();
+
+            try
+            {
+                var phoneNumberParsed = phoneNumberUtil.Parse(phone, model.CountryCode);
+
+                return phoneNumberUtil.IsPossibleNumberForType(phoneNumberParsed, PhoneNumberType.MOBILE);
+
+            }
+            catch
+            {
+                return false;
+            }
+
+        }
     }
 }
--- MicroStore.Profiling.Application/Models/ProfileModel.cs
+++ MicroStore.Profiling.Application/Models/ProfileModel.cs
@@ -1,4 +1,7 @@
-﻿using MicroStore.Profiling.Application.Domain;
+﻿using FluentValidation;
+using MicroStore.Profiling.Application.Domain;
+using PhoneNumbers;
+using Volo.Abp.Domain.Repositories;
 namespace MicroStore.Profiling.Application.Models
 {
 
@@ -10,11 +13,105 @@ namespace MicroStore.Profiling.Application.Models
         public string Gender { get; set; }
         public PhoneModel Phone { get; set; }
         public string? Avatar { get; set; }
-        public List<Address>? Addresses { get; set; }
+        public List<AddressModel>? Addresses { get; set; }
     }
 
     public class CreateProfileModel : ProfileModel
     {
         public string UserId { get; set; }
+    }
+
+
+    internal class ProfileModelValidationBase<T> : AbstractValidator<T> where T : ProfileModel
+    {
+        const string URI_REGEX_PATTERN = @"(https:[/][/]|http:[/][/]|www.)[a-zA-Z0-9\\-\\.]+\\.[a-zA-Z]{2,3}(:[a-zA-Z0-9]*)?/?([a-zA-Z0-9\\-\\._\\?\\,\\'/\\\\\\+&amp;%\\$#\\=~])*$";
+
+        public ProfileModelValidationBase()
+        {
+            RuleFor(x => x.FirstName)
+                .NotNull()
+                .WithMessage("First Name is required")
+                .MaximumLength(256);
+
+            RuleFor(x => x.LastName)
+                .NotNull()
+                .WithMessage("Last Name is required")
+                .MaximumLength(256);
+
+
+            RuleFor(x => x.BirthDate)
+                .Must((date) => date <= DateTime.Now.AddYears(-12))
+                .WithMessage("Invalid birth date");
+
+
+            RuleFor(x => x.Phone)
+                .ChildRules(child =>
+                {
+
+                    child.RuleFor(x => x.Number)
+                         .NotNull()
+                         .WithMessage("Phone Number is required.")
+                         .MinimumLength(10)
+                         .WithMessage("Phone Number must not be less than 10 characters.")
+                         .MaximumLength(50)
+                         .WithMessage("Phone Number must not exceed 50 characters.")
+                         .Must((model, _) => ValidatePhone(model.Number, model.CountryCode))
+                         .WithMessage("Invalid phone number");
+
+                    child.RuleFor(x => x.CountryCode)
+                     .NotNull()
+                     .WithMessage("Country Code is required")
+                     .MinimumLength(2)
+                     .WithMessage("Country Code minimum lenght is 2")
+                     .MaximumLength(3)
+                     .WithMessage("Country Code maximum length is 3");
+                });
+
+
+            RuleForEach(x => x.Addresses)
+                .SetValidator(new AddressValidator())
+                .When(x => x.Addresses != null);
+
+        }
+
+        private bool ValidatePhone(string number, string countryCode)
+        {
+            var phoneNumberUtil = PhoneNumberUtil.GetInstance();
+
+            try
+            {
+                var phoneNumberParsed = phoneNumberUtil.Parse(number, countryCode);
+
+                return phoneNumberUtil.IsPossibleNumberForType(phoneNumberParsed, PhoneNumberType.MOBILE);
+
+            }
+            catch
+            {
+                return false;
+            }
+
+        }
+    }
+
+
+    internal class ProfileModelValidation : ProfileModelValidationBase<ProfileModel> { }
+
+    internal class CreateProfileModelValidation : ProfileModelValidationBase<CreateProfileModel>
+    {
+
+        private readonly IRepository<Profile> _profileRepository;
+
+        public CreateProfileModelValidation(IRepository<Profile> profileRepository)
+        {
+            _profileRepository = profileRepository;
+
+            RuleFor(x => x.UserId)
+                .NotNull()
+                .WithMessage("User Id is required")
+                .MaximumLength(256)
+                .MustAsync(async (userId, ct) => await _profileRepository.AllAsync(x => x.UserId != userId))
+                .WithMessage("User already has profile");
+          
+        }
     }
 }
