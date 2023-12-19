a4d966140225ff91a000ac54d85fceca8ee47804
Mohamed Gamal <mohamedsherf17@gmail.com>
2023-12-19 22:04:59 +0200

    Fix Basket Aggregation Service Retrievement Preformance Issue

--- MicroStore.ShoppingGateway.ClinetSdk/Services/Cart/BasketAggregateService.cs
+++ MicroStore.ShoppingGateway.ClinetSdk/Services/Cart/BasketAggregateService.cs
@@ -1,6 +1,5 @@
 ﻿using MicroStore.ShoppingGateway.ClinetSdk.Entities.Cart;
 using MicroStore.ShoppingGateway.ClinetSdk.Entities.Catalog;
-using MicroStore.ShoppingGateway.ClinetSdk.Extensions;
 using MicroStore.ShoppingGateway.ClinetSdk.Services.Catalog;
 namespace MicroStore.ShoppingGateway.ClinetSdk.Services.Cart
 {
@@ -27,23 +26,42 @@ namespace MicroStore.ShoppingGateway.ClinetSdk.Services.Cart
 
             var basketResponse = await _basketService.RetrieveAsync(userId, cancellationToken: cancellationToken);
 
+
             if(basketResponse != null)
             {
-                foreach (var item in basketResponse.Items)
-                {
-                    var productResponse = await _productService.GetAsync(item.ProductId, cancellationToken: cancellationToken);
+                var tasks = basketResponse.Items
+                    .Select(x => PreapreBasketItemAggregate(x, cancellationToken));
 
+                var items =  await Task.WhenAll(tasks);
 
-                    var basketItem = PrepareBasketItemAggregate(productResponse);
+                basketAggregate.Items.AddRange(items);
 
-                    basketItem.Quantity = item.Quantity;
-
-                    basketAggregate.Items.Add(basketItem);
-                }
             }
             return basketAggregate;
         }
 
+
+        private async Task<BasketItemAggregate> PreapreBasketItemAggregate(BasketItem item, CancellationToken cancellationToken)
+        {
+            var product = await _productService.GetAsync(item.ProductId, cancellationToken: cancellationToken);
+
+            var itemAggregate =  new BasketItemAggregate
+            {
+                ProductId = product.Id.ToString(),
+                ShortDescription = product.ShortDescription,
+                LongDescription = product.LongDescription,
+                Name = product.Name,
+                Sku = product.Sku,
+                Thumbnail = product.ProductImages.FirstOrDefault()?.Image,
+                Price = product.Price,
+                Weight = product.Weight,
+                Dimensions = product.Dimensions,
+                Quantity = item.Quantity
+            };
+
+
+            return itemAggregate;
+        }
 
 
         private BasketItemAggregate PrepareBasketItemAggregate(Product product)
