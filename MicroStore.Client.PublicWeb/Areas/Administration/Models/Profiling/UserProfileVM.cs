bdd1b5ccfc6fb31780dd1992b4727ae709ac67be
Mohamed Gamal <mohamedsherf17@gmail.com>
2023-12-24 00:49:30 +0200

    Add Customer Information Section To Order Details

--- MicroStore.Client.PublicWeb/Areas/Administration/Models/Profiling/UserProfileVM.cs
+++ MicroStore.Client.PublicWeb/Areas/Administration/Models/Profiling/UserProfileVM.cs
@@ -1,4 +1,6 @@
 ﻿using MicroStore.ShoppingGateway.ClinetSdk.Services.Profiling;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace MicroStore.Client.PublicWeb.Areas.Administration.Models.Profiling
 {
@@ -6,11 +8,23 @@ namespace MicroStore.Client.PublicWeb.Areas.Administration.Models.Profiling
     {
         public string Id { get; set; }
         public string UserId { get; set; }
+
+        [DisplayName("First Name")]
         public string FirstName { get; set; }
+
+        [DisplayName("Last Name")]
         public string LastName { get; set; }
+
+        [DisplayName("Birth Date")]
         public DateTime BirthDate { get; set; }
+
+        [DisplayName("Gender")]
         public Gender Gender { get; set; }
+
+        [DisplayName("Phone")]
         public string Phone { get; set; }
+
+        [DisplayName("Avatar")]
         public string Avatar { get; set; }
         public List<AddressEntityVM> Addresses { get; set; }
     }
