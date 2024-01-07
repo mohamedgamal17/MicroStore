4999ac9f45cba870f744d74b1754e45313c3c319
Mohamed Gamal <mohamedsherf17@gmail.com>
2024-01-07 03:01:23 +0200

    Add Geogrpahic Grpc Routes

---
+++ MicroStore.Geographic.Host/Grpc/CountryGrpcService.cs
@@ -0,0 +1,182 @@
+﻿using FluentValidation;
+using FluentValidation.Results;
+using Grpc.Core;
+using MicroStore.Geographic.Application.Dtos;
+using MicroStore.Geographic.Application.Models;
+using Volo.Abp.DependencyInjection;
+using MicroStore.BuildingBlocks.AspNetCore.Extensions;
+using MicroStore.Geographic.Application.Countries;
+using Google.Protobuf.WellKnownTypes;
+namespace MicroStore.Geographic.Host.Grpc
+{
+    public class CountryGrpcService : CountryService.CountryServiceBase
+    {
+        private readonly ICountryApplicationService _countryApplicationService;
+
+        public CountryGrpcService(ICountryApplicationService countryApplicationService)
+        {
+            _countryApplicationService = countryApplicationService;
+        }
+
+        public IAbpLazyServiceProvider LazyServiceProvider { get; set; }
+
+        public override async Task<CountryResponse> Create(CreateCountryRequest request, ServerCallContext context)
+        {
+            var model = PrepareCountryMode(request);
+
+            var validationResult = await ValidateModel(model);
+
+            if (!validationResult.IsValid)
+            {
+                validationResult.ThrowRpcException();
+            }
+
+            var result = await _countryApplicationService.CreateAsync(model);
+
+            if (result.IsFailure)
+            {
+                result.Exception.ThrowRpcException();
+            }
+
+            return PrepareCountryResponse(result.Value);
+        }
+
+        public override async Task<CountryResponse> Update(UpdateCountryRequest request, ServerCallContext context)
+        {
+            var model = PrepareCountryMode(request);
+
+            var validationResult = await ValidateModel(model);
+
+            if (!validationResult.IsValid)
+            {
+                validationResult.ThrowRpcException();
+            }
+
+            var result = await _countryApplicationService.UpdateAsync(request.Id,model);
+
+            if (result.IsFailure)
+            {
+                result.Exception.ThrowRpcException();
+            }
+
+            return PrepareCountryResponse(result.Value);
+        }
+
+        public override async Task<CountryListResponse> GetList(CountryListRequest request, ServerCallContext context)
+        {
+            var result = await _countryApplicationService.ListAsync();
+
+            return PrepareCountryListResponse(result);
+        }
+
+        public override async Task<CountryResponse> GetById(GetCountryByIdRequest request, ServerCallContext context)
+        {
+            var result = await _countryApplicationService.GetAsync(request.Id);
+
+            if (result.IsFailure)
+            {
+                result.Exception.ThrowRpcException();
+            }
+
+            return PrepareCountryResponse(result.Value);
+
+        }
+
+        public override async Task<CountryResponse> GetByCode(GetCountryByCodeRequest request, ServerCallContext context)
+        {
+            var result = await _countryApplicationService.GetByCodeAsync(request.Code);
+
+            if (result.IsFailure)
+            {
+                result.Exception.ThrowRpcException();
+            }
+
+            return PrepareCountryResponse(result.Value);
+        }
+
+        private CountryModel PrepareCountryMode(CreateCountryRequest request)
+        {
+            return new CountryModel
+            {
+                Name = request.Name,
+                TwoLetterIsoCode = request.TwoLetterIsoCode,
+                ThreeLetterIsoCode = request.ThreeLetterIsoCode,
+                NumericIsoCode = request.NumericIsoCode
+            };
+        }
+
+        private CountryModel PrepareCountryMode(UpdateCountryRequest request)
+        {
+            return new CountryModel
+            {
+                Name = request.Name,
+                TwoLetterIsoCode = request.TwoLetterIsoCode,
+                ThreeLetterIsoCode = request.ThreeLetterIsoCode,
+                NumericIsoCode = request.NumericIsoCode
+            };
+        }
+
+        private CountryResponse PrepareCountryResponse(CountryDto country)
+        {
+            var respoonse = new CountryResponse
+            {
+                Id = country.Id,
+                Name = country.Name,
+                TwoLetterIsoCode = country.TwoLetterIsoCode,
+                ThreeLetterIsoCode = country.ThreeLetterIsoCode,
+                NumericIsoCode = country.NumericIsoCode,
+            };
+
+
+            country.StateProvinces.ForEach(state =>
+            {
+                var stateResponse = new StateProvinceResponse
+                {
+                    Id = state.Id,
+                    Name = state.Name,
+                    Abbrevation = state.Abbreviation,
+                    CreatedAt = Timestamp.FromDateTime(state.CreationTime)
+                };
+
+                respoonse.States.Add(stateResponse);
+            });
+
+            return respoonse;
+        }
+
+        private CountryListResponse PrepareCountryListResponse(List<CountryListDto> countries)
+        {
+            var response = new CountryListResponse();
+            countries.ForEach(x =>
+            {
+                var country = new CountryResponse
+                {
+                    Id = x.Id,
+                    Name = x.Name,
+                    TwoLetterIsoCode = x.TwoLetterIsoCode,
+                    ThreeLetterIsoCode = x.ThreeLetterIsoCode,
+                    NumericIsoCode = x.NumericIsoCode
+                };
+
+
+                response.Items.Add(country);
+            });
+
+
+            return response;
+        }
+
+        private async Task<ValidationResult> ValidateModel<TModel>(TModel model)
+        {
+            var validator = ResolveValidator<TModel>();
+
+            if (validator == null) return new ValidationResult();
+
+            return await validator.ValidateAsync(model);
+        }
+        private IValidator<T>? ResolveValidator<T>()
+        {
+            return LazyServiceProvider.LazyGetService<IValidator<T>>();
+        }
+    }
+}
---
+++ MicroStore.Geographic.Host/Grpc/StateProvinceService.cs
@@ -0,0 +1,150 @@
+﻿using FluentValidation;
+using FluentValidation.Results;
+using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using MicroStore.Geographic.Application.Dtos;
+using MicroStore.Geographic.Application.Models;
+using Volo.Abp.DependencyInjection;
+using MicroStore.BuildingBlocks.AspNetCore.Extensions;
+using MicroStore.Geographic.Application.StateProvinces;
+namespace MicroStore.Geographic.Host.Grpc
+{
+    public class StateProvinceGrpcService : StateProvinceService.StateProvinceServiceBase
+    {
+        private readonly IStateProvinceApplicationService _stateProvinceApplicationService;
+
+        public StateProvinceGrpcService(IStateProvinceApplicationService stateProvinceApplicationService)
+        {
+            _stateProvinceApplicationService = stateProvinceApplicationService;
+        }
+
+        public IAbpLazyServiceProvider LazyServiceProvider { get; set; }
+
+        private StateProvinceResponse PrepareStateProvinceResponse(StateProvinceDto stateProvince)
+        {
+            return new StateProvinceResponse
+            {
+                Id = stateProvince.Id,
+                Name = stateProvince.Name,
+                Abbrevation = stateProvince.Abbreviation,
+                CreatedAt = Timestamp.FromDateTime(stateProvince.CreationTime)
+            };
+        }
+
+        public override async Task<StateProvinceResponse> Create(CreateStateProvinceRequest request, ServerCallContext context)
+        {
+            var model = PrepareStateProvinceModel(request);
+
+            var validationResult = await ValidateModel(model);
+
+            if (!validationResult.IsValid)
+            {
+                validationResult.ThrowRpcException();
+            }
+
+            var result = await _stateProvinceApplicationService.CreateAsync(request.CountryId, model);
+
+            return PrepareStateProvinceResponse(result.Value);
+        }
+
+        public override async Task<StateProvinceResponse> Update(UpdateStateProvinceRequest request, ServerCallContext context)
+        {
+            var model = PrepareStateProvinceModel(request);
+
+            var validationResult = await ValidateModel(model);
+
+            if (!validationResult.IsValid)
+            {
+                validationResult.ThrowRpcException();
+            }
+
+            var result = await _stateProvinceApplicationService.UpdateAsync(request.CountryId, request.StateProvinceId,model);
+
+            if (result.IsFailure)
+            {
+                result.Exception.ThrowRpcException();
+            }
+
+            return PrepareStateProvinceResponse(result.Value);
+        }
+
+        public override async Task<StateProvinceListResponse> GetList(StateProvinceListRequest request, ServerCallContext context)
+        {
+            var result = await _stateProvinceApplicationService.ListAsync(request.CountryId);
+
+            if (result.IsFailure)
+            {
+                result.Exception.ThrowRpcException();
+            }
+
+            return PrepareStateProvinceListResponse(result.Value);
+        }
+
+        public override async Task<StateProvinceResponse> GetById(GetStateProvinceByIdRequest request, ServerCallContext context)
+        {
+            var result = await _stateProvinceApplicationService.GetAsync(request.CountryId,request.StateProvinceId);
+
+            if (result.IsFailure)
+            {
+                result.Exception.ThrowRpcException();
+            }
+
+
+            return PrepareStateProvinceResponse(result.Value);
+        }
+
+        public override async Task<StateProvinceResponse> GetByCode(GetStateProvinceByCodeRequest request, ServerCallContext context)
+        {
+            var result = await _stateProvinceApplicationService.GetByCodeAsync(request.CountryCode, request.StateProvinceCode);
+
+            if (result.IsFailure)
+            {
+                result.Exception.ThrowRpcException();
+            }
+
+            return PrepareStateProvinceResponse(result.Value);
+        }
+
+        private StateProvinceModel PrepareStateProvinceModel(CreateStateProvinceRequest request)
+        {
+            return new StateProvinceModel
+            {
+                Name = request.Name,
+                Abbreviation = request.Abbrevation
+            };
+        }
+
+        private StateProvinceModel PrepareStateProvinceModel(UpdateStateProvinceRequest request)
+        {
+            return new StateProvinceModel
+            {
+                Name = request.Name,
+                Abbreviation = request.Abbrevation
+            };
+        }
+        private StateProvinceListResponse PrepareStateProvinceListResponse(List<StateProvinceDto> stateProvinces)
+        {
+            var response = new StateProvinceListResponse();
+
+            stateProvinces.ForEach(state =>
+            {
+                response.Items.Add(PrepareStateProvinceResponse(state));
+            });
+
+            return response;
+        }
+
+        private async Task<ValidationResult> ValidateModel<TModel>(TModel model)
+        {
+            var validator = ResolveValidator<TModel>();
+
+            if (validator == null) return new ValidationResult();
+
+            return await validator.ValidateAsync(model);
+        }
+        private IValidator<T>? ResolveValidator<T>()
+        {
+            return LazyServiceProvider.LazyGetService<IValidator<T>>();
+        }
+    }
+}
