ab115dddeb1a2b1f522c6f6d77654b0f3dffb03b
Mohamed Gamal <mohamedsherf17@gmail.com>
2024-01-24 04:17:44 +0200

    Add ApplicationCurrentUser

---
+++ MicroStore.Bff.Shopping/Infrastructure/ApplicationCurrentUser.cs
@@ -0,0 +1,22 @@
+﻿using IdentityModel;
+using Microsoft.AspNetCore.Http;
+using Volo.Abp.DependencyInjection;
+
+namespace MicroStore.Bff.Shopping.Infrastructure
+{
+    public class ApplicationCurrentUser
+    {
+        private readonly HttpContext httpContext;
+        public ApplicationCurrentUser(IHttpContextAccessor httpContextAccessor)
+        {
+            httpContext = httpContextAccessor.HttpContext!;
+        }
+        public string? UserId => FindClaimValue(JwtClaimTypes.Subject);
+        public string? UserName => FindClaimValue(JwtClaimTypes.Name);
+        public string? Email => FindClaimValue(JwtClaimTypes.Email);
+        private string? FindClaimValue(string type)
+        {
+            return httpContext.User?.Claims.SingleOrDefault(x => x.Type == type)?.Value;
+        }
+    }
+}
