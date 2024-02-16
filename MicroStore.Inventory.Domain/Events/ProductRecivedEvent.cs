99c0d1e407e9992c3287e3f407ac32cb4afe660c
Mohamed Gamal <mohamedsherf17@gmail.com>
2024-02-16 20:49:56 +0200

    Remove Old Files

--- MicroStore.Inventory.Domain/Events/ProductRecivedEvent.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace MicroStore.Inventory.Domain.Events
-{
-    public class ProductRecivedEvent : EventBase
-    {
-        public int RecivedQuantity { get; init; }
-        public DateTime RecivedDate { get; init; }
-    }
-}
--- MicroStore.Inventory.Domain/Events/ProductShippedEvent.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MicroStore.Inventory.Domain.Events
-{
-    public class ProductShippedEvent : EventBase
-    {
-        public int ShippedQuantity { get; init; }
-
-        public DateTime ShippedDate { get; init; }
-    }
-}
